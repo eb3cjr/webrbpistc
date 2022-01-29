2d8d52aef1dae4a04a9356fe56ecdd12303f0405
roguib <rgrguib@gmail.com>
2022-01-29 17:01:47 +0100

    max values

--- WEB_STC/db/metricsRepository.js
+++ WEB_STC/db/metricsRepository.js
@@ -10,15 +10,61 @@ module.exports = class MetricsRepository {
     static async getRaspberryLatestsMetrics() {
         // we supose nothing bad happens so we don't handle possible errors
         const dbCon = await MetricsRepository._connectToDB();
-        const statement = `SELECT (cpu_temp AS cpuTemp, psu_vol_sol_1 AS psuVolSol1,
+        const statement = `SELECT cpu_temp AS cpuTemp, psu_vol_sol_1 AS psuVolSol1,
             psu_vol_bat_1 AS psuVolBat1, psu_vol_bat_2 AS psuVolBat2,
-            psu_vol_rb_pi AS psuVolRbPi, timestamp)
+            psu_vol_rb_pi AS psuVolRbPi, timestamp
             FROM stc_bat_dades
             ORDER BY timestamp DESC;`;
 
         const rows = await MetricsRepository._promisifyAll(dbCon, statement);
         dbCon.close();
         return rows[0];
+    }
+
+    /**
+     * Returns the max values in the database.
+     */
+    static async getRaspberryMaxMetrics() {
+        // we supose nothing bad happens so we don't handle possible errors
+        const dbCon = await MetricsRepository._connectToDB();
+        // const statement = `SELECT MAX(cpu_temp) AS cpuTempMax, timestamp AS cpuTempTimestampMax
+        // FROM stc_bat_dades
+        // SELECT MAX(psu_vol_sol_1) AS psuVolSol1Max, timestamp AS psuVolSol1TimestampMax
+        // FROM stc_bat_dades;`;
+        const statementCpuMax = `SELECT MAX(cpu_temp) AS cpuTempMax, timestamp AS timestamp
+        FROM stc_bat_dades;`;
+        const rowsCpuMax = await MetricsRepository._promisifyAll(dbCon, statementCpuMax);
+        const statementPsuVol1Max = `SELECT MAX(psu_vol_sol_1) AS psuVolSol1Max, timestamp AS timestamp
+        FROM stc_bat_dades;`;
+        const rowsPsuVol1Max = await MetricsRepository._promisifyAll(dbCon, statementPsuVol1Max);
+        const statementPsuBat1Max = `SELECT MAX(psu_vol_bat_1) AS psuVolBat1Max, timestamp AS timestamp
+        FROM stc_bat_dades;`;
+        const rowsPsuBat1Max = await MetricsRepository._promisifyAll(dbCon, statementPsuBat1Max);
+        const statementPsuBat2Max = `SELECT MAX(psu_vol_bat_2) AS psuVolBat2Max, timestamp AS timestamp
+        FROM stc_bat_dades;`;
+        const rowsPsuBat2Max = await MetricsRepository._promisifyAll(dbCon, statementPsuBat2Max);
+        const statementPsuVolRbPiMax = `SELECT MAX(psu_vol_rb_pi) AS psuVolRbPiMax, timestamp AS timestamp
+        FROM stc_bat_dades;`;
+        const rowsPsuVolRbPiMax = await MetricsRepository._promisifyAll(dbCon, statementPsuVolRbPiMax);
+        const data = {
+            cpuTempMax: {
+                ...rowsCpuMax[0]
+            },
+            psuVol1Max: {
+                ...rowsPsuVol1Max[0]
+            },
+            psuBat1Max: {
+                ...rowsPsuBat1Max[0]
+            },
+            psuBat2Max: {
+                ...rowsPsuBat2Max[0]
+            },
+            psuRbPiMax: {
+                ...rowsPsuVolRbPiMax[0]
+            }
+        };
+        console.log(data);
+        return data;
     }
 
     /**
@@ -46,7 +92,7 @@ module.exports = class MetricsRepository {
         return new Promise((resolve, reject) => {
             dbCon.all(statement, [], (err, rows) => {
                 if (err) {
-                  reject(error);
+                  reject(err);
                 }
                 resolve(rows);
             });
--- WEB_STC/index.js
+++ WEB_STC/index.js
@@ -16,6 +16,8 @@ const port = 8090
 app.get('/', async (req, res) => {
   const { cpuTemp, psuVolSol1, psuVolBat1, psuVolBat2, psuVolRbPi, timestamp } =
     await MetricsRepository.getRaspberryLatestsMetrics();
+  const { cpuTempMax, psuVol1Max, psuBat1Max, psuBat2Max, psuRbPiMax } =
+    await MetricsRepository.getRaspberryMaxMetrics();
   res.render(path.join(__dirname,'./views/view'), {
     author: 'Súper RG & RR',
     date: '06_01_2022',
@@ -24,9 +26,28 @@ app.get('/', async (req, res) => {
     psuVolBat1,
     psuVolBat2,
     psuVolRbPi,
-    timestamp
+    timestamp,
+    cpuTempMax: {
+      ...cpuTempMax,
+      timestamp: new Date(cpuTempMax.timestamp)
+    },
+    psuVol1Max: {
+      ...psuVol1Max,
+      timestamp: new Date(psuVol1Max.timestamp)
+    },
+    psuBat1Max: {
+      ...psuBat1Max,
+      timestamp: new Date(psuBat1Max.timestamp)
+    },
+    psuBat2Max: {
+      ...psuBat2Max,
+      timestamp: new Date(psuBat2Max.timestamp)
+    },
+    psuRbPiMax: {
+      ...psuRbPiMax,
+      timestamp: new Date(psuRbPiMax.timestamp)
+    }
   });
-
 })
 
 app.listen(port, () => {
